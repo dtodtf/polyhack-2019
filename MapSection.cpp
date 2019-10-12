dfd88ad8a3331d9238218405e02ccc05ad927b2c
a-delma <ap.delmastro@gmail.com>
2019-10-11 20:52:55 -0400

    test

---
+++ MapSection.cpp
@@ -0,0 +1,11 @@
+/*
+ * This would manage the section of the map we restrict ourselves to
+ * Arith this file will transpose images into compressed files
+ * This implementation would assume that we have already sectioned off a 
+ * portion of the map
+ * 
+ * 
+ */
+
+#include "MapSection.h"
+#include <climits>
---
+++ MapSection.h
@@ -0,0 +1,42 @@
+/*
+ * Anyone remember how to make header files for cpp
+ * no
+ * 
+ * 
+ * 
+ */
+
+#ifndef MAPSECTION_H_
+#define MAPSECTION_H_
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+class MapSection {
+
+public:
+
+
+
+
+
+private:
+
+    // Values for dimensions of the section of map
+    // Would be constant for our cases, but good to have access to. 
+    int height;
+    int width;
+
+    // do we want values where we can store the coordinates of the outer c
+    // corners of our map section?
+
+
+};
+
+#endif 
